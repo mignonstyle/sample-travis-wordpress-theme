4ef81a4c698630c31494fe0cdfc5bc2d50e41989
mignonstyle <mignonxstyle@gmail.com>
2016-10-14 16:26:34 +0900

    Modify gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp         = require( 'gulp' );
 var changed      = require( 'gulp-changed' );
 var concat       = require( 'gulp-concat' );
 var csso         = require( 'gulp-csso' );
-var imagemin     = require( 'gulp-imagemin' );
+//var imagemin     = require( 'gulp-imagemin' );
 var plumber      = require( 'gulp-plumber' );
 var sass         = require( 'gulp-sass' );
 var uglify       = require( 'gulp-uglify' );
 var rename       = require( 'gulp-rename' );
-//var dir          = require( 'require-dir' );
 var watch        = require( 'gulp-watch' );
 var autoprefixer = require( 'gulp-autoprefixer' );
+var requireDir   = require( 'require-dir' );
 var browserSync  = require( 'browser-sync' );
 
 gulp.task( 'default', function () {
